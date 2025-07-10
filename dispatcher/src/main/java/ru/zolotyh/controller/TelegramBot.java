992d9c3213323dd2b60bb8066c019bf6eeaf4275
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-07-10 09:17:07 +0300

    Add sending a reply message from the bot to the chat

--- dispatcher/src/main/java/ru/zolotyh/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/zolotyh/controller/TelegramBot.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 @Component
@@ -31,5 +33,20 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         Message originalMessage = update.getMessage();
         log.debug(originalMessage.getText());
+
+        SendMessage response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello from bot");
+        sendAnswerMessage(response);
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
     }
 }
