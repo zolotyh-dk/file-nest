cba45bceb4f5d76554e1fdce7b6f2583126f4347
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-07-22 10:02:02 +0300

    Add a service layer for messaging with a broker

--- dispatcher/src/main/java/ru/zolotyh/controller/UpdateController.java
+++ dispatcher/src/main/java/ru/zolotyh/controller/UpdateController.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.zolotyh.service.UpdateProducer;
 import ru.zolotyh.utils.MessageUtils;
+
+import static ru.zolotyh.model.RabbitQueue.*;
 
 @Component
 @Slf4j
 public class UpdateController {
     private TelegramBot telegramBot;
+    private UpdateProducer updateProducer;
 
     public void registerBot(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
@@ -43,15 +47,15 @@ public class UpdateController {
     }
 
     private void processTextMessage(Update update) {
-
+        updateProducer.produce(TEXT_MESSAGE_UPDATE, update);
     }
 
     private void processDocMessage(Update update) {
-
+        updateProducer.produce(DOC_MESSAGE_UPDATE, update);
     }
 
     private void processPhotoMessage(Update update) {
-
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE, update);
     }
 
     private void setUnsupportedMessageTypeView(Update update) {
---
+++ dispatcher/src/main/java/ru/zolotyh/service/UpdateProducerImpl.java
@@ -0,0 +1,14 @@
+package ru.zolotyh.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+@Slf4j
+public class UpdateProducerImpl implements UpdateProducer {
+    @Override
+    public void produce(String rabbitQueue, Update update) {
+        log.debug(update.getMessage().getText());
+    }
+}
