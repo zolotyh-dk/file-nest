1e02b11f4ea01f1f7ee89833c120d4938e3bc838
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-07-28 12:44:59 +0300

    Add a configuration class for RabbitMQ

---
+++ dispatcher/src/main/java/ru/zolotyh/config/RabbitConfiguration.java
@@ -0,0 +1,38 @@
+package ru.zolotyh.config;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static ru.zolotyh.model.RabbitQueue.*;
+
+@Configuration
+public class RabbitConfiguration {
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue textMessageQueue() {
+        return new Queue(TEXT_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue docMessageQueue() {
+        return new Queue(DOC_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue photoMessageQueue() {
+        return new Queue(PHOTO_MESSAGE_UPDATE);
+    }
+
+    @Bean
+    public Queue answertMessageQueue() {
+        return new Queue(ANSWER_MESSAGE);
+    }
+}
